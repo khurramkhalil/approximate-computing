8212e6e673e3ae9fc78245de8016e18189253167
Khurram Khalil <khurram.khalil@missouri.edu>
2024-09-12 16:51:21 -0500

    Reimplemented technique with both uncertanity and KS test.

--- examples/models/SDNs/sdm_fault_mitigation.py
+++ examples/models/SDNs/sdm_fault_mitigation.py
@@ -1,9 +1,12 @@
 import torch
 import random
 import numpy as np
+from tqdm import tqdm
 from scipy import stats
 from models.SDNs import data
+from adapt.approx_layers import axx_layers as approxNN
 
+import pdb
 # class StatisticalDistributionMonitoring:
 #     def __init__(self, model, sample_size=100, significance_level=0.05, check_frequency=100):
 #         self.model = model
@@ -54,9 +57,8 @@ from models.SDNs import data
 #         param.data[mask] = torch.normal(mean, std, size=param.data[mask].shape)
 
 class StatisticalDistributionMonitoring:
-    def __init__(self, model, sample_size=1000, significance_level=0.05, check_frequency=100):
+    def __init__(self, model,significance_level=0.10, check_frequency=100):
         self.model = model
-        self.sample_size = sample_size
         self.significance_level = significance_level
         self.check_frequency = check_frequency
         self.initial_stats = self.compute_initial_stats()
@@ -64,13 +66,15 @@ class StatisticalDistributionMonitoring:
 
     def compute_initial_stats(self):
         stats = {}
-        for name, module in self.model.named_modules():
-            if isinstance(module, torch.nn.Conv2d) or isinstance(module, torch.nn.Linear):
-                weights = module.weight.data.cpu().numpy().flatten()
+        for name, parameter in self.model.named_parameters():
+            # if isinstance(module, torch.nn.Conv2d) or isinstance(module, torch.nn.Linear) or isinstance(module, approxNN.AdaPT_Conv2d) or isinstance(module, approxNN.AdaPT_Linear):
+            if 'weight' in name:
+                weights = parameter.data.cpu().numpy().flatten()
+
                 stats[name] = {
                     'mean': np.mean(weights),
                     'std': np.std(weights),
-                    'sample': np.random.choice(weights, size=self.sample_size, replace=False)
+                    'sample': np.random.choice(weights, size=len(weights), replace=False)
                 }
         return stats
 
@@ -78,13 +82,14 @@ class StatisticalDistributionMonitoring:
         if layer_name not in self.initial_stats:
             return False
 
-        module = dict(self.model.named_modules())[layer_name]
-        current_weights = module.weight.data.cpu().numpy().flatten()
-        current_sample = np.random.choice(current_weights, size=self.sample_size, replace=False)
+        parameter = dict(self.model.named_parameters())[layer_name]
+        current_weights = parameter.data.cpu().numpy().flatten()
+
+        current_sample = np.random.choice(current_weights, size=len(current_weights), replace=False)
         
-        ks_statistic, p_value = stats.ks_2samp(self.initial_stats[layer_name]['sample'], current_sample)
+        result = stats.anderson_ksamp([self.initial_stats[layer_name]['sample'], current_sample])
         
-        return p_value < self.significance_level
+        return result.significance_level < self.significance_level
 
     def check_distributions(self):
         affected_layers = []
@@ -100,17 +105,20 @@ class StatisticalDistributionMonitoring:
         return []
 
 def targeted_fault_mitigation(model, layer_name):
-    module = dict(model.named_modules())[layer_name]
+    parameter = dict(model.named_parameters())[layer_name]
     with torch.no_grad():
-        mean = module.weight.data.mean()
-        std = module.weight.data.std()
+        mean = parameter.data.mean()
+        std = parameter.data.std()
         # Replace extreme outliers with values drawn from a normal distribution
-        mask = torch.abs(module.weight.data - mean) > 3 * std
-        module.weight.data[mask] = torch.normal(mean, std, size=module.weight.data[mask].shape)
+        mask = torch.abs(parameter.data - mean) > 3 * std
+        # parameter.data[mask] = torch.normal(mean, std, size=parameter.data[mask].shape)
+        parameter.data[mask] = 0
 
-def sdn_test_early_exits_sdm(model, loader, confidence_threshold=0.5, device='cpu'):
-    model.forward = model.early_exit_only
+def sdn_test_early_exits_sdm(model, loader, confidence_threshold=0.5, uncertainty_threshold=None, device='cpu'):
+    model.forward = model.early_exit
+    # model.forward = model.early_exit_only
     model.confidence_threshold = confidence_threshold
+    model.uncertainty_threshold = uncertainty_threshold
 
     # sdm = StatisticalDistributionMonitoring(model)
 
@@ -126,7 +134,8 @@ def sdn_test_early_exits_sdm(model, loader, confidence_threshold=0.5, device='cp
     fault_mitigations = 0
 
     with torch.no_grad():
-        for batch in loader:
+        # Wrap the loader with tqdm for progress bar
+        for batch in tqdm(loader, desc="Testing", leave=False):
             b_x = batch[0].to(device)
             b_y = batch[1].to(device)
             
@@ -164,7 +173,7 @@ def introduce_fault_with_sdm(model, percent_of_faults, fault_loc=None, layer_to_
     sdm = StatisticalDistributionMonitoring(model)
     model.eval()
     for name, param in model.named_parameters():
-        if layer_to_attack == name:
+        if name in layer_to_attack: 
             print("Attacked layer", name)
             print(param.shape)
             w1 = param.data
@@ -176,15 +185,18 @@ def introduce_fault_with_sdm(model, percent_of_faults, fault_loc=None, layer_to_
             print("Number of weights attacked", no_of_faults)
             if fault_loc is None:
                 fault_loc = random.sample(range(0, len(wf1)), no_of_faults)
+                fault = [random.uniform(-2, 2) for _ in range(len(fault_loc))]
             for i in range(0, len(fault_loc)):
-                wf1[fault_loc[i]] = -wf1[fault_loc[i]]
+                # wf1[fault_loc[i]] = -wf1[fault_loc[i]]
+                wf1[fault_loc[i]] = torch.tensor(fault[i])
             wf11 = wf1.reshape(w1.shape)
             param.data = wf11
     
-    affected_layer = sdm.check_distributions()
-    correction = False
-    if affected_layer:
-        targeted_fault_mitigation(model, affected_layer)
-        correction = True
+    affected_layers = sdm.check_distributions()
+    correction = 0
+    if affected_layers:
+        for affected_layer in affected_layers:
+            targeted_fault_mitigation(model, affected_layer)
+            correction += 1
     
     return model, correction
